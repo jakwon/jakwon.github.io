b0c1f9d255034993e67049379737856e713d9e14
JKwon <kwon.janet@gmail.com>
2015-12-03 18:26:48 -0500

    final
    final, although not done

--- js/index.js
+++ js/index.js
@@ -5,4 +5,17 @@ $(function(){
     $('.responsive-menu').toggleClass('expand')
   })
 
+$("#slideshow > div:gt(0)").hide();
+
+setInterval(function() { 
+  $('#slideshow > div:first')
+    .fadeOut(2000)
+    .next()
+    .delay(500)
+    .fadeIn(2000)
+    .end()
+    .appendTo('#slideshow');
+},  4000);
+
+
 });
